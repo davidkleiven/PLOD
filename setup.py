74ca25091c921c4cd7eed0ef626efe28ce82ce9f
David Kleiven <davidkleiven446@gmail.com>
2016-12-25 10:10:05 -0500

    Seem to work

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from disutils.core import setup
+from setuptools import setup
 
 setup(
     name='PLOD',
     version='1.0',
     description='Matplotlib plot designer',
     author='David Kleiven',
+    licence='MIT',
     author_email='davidkleiven446@gmail.com',
+    install_requires=['numpy', 'matplotlib','subprocess','tkinter'],
     url='https://github.com/davidkleiven/PLOD',
-    py_modules=['plotHandler', 'controlGUI']
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    #py_modules=['plotHandler', 'controlGUI'],
+    packages=['PLOD']
 )
