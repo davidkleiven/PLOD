6d5dbb4a2651e0019d40a5cfbfb70d1bbe4f0871
David Kleiven <davidkleiven446@gmail.com>
2016-12-25 09:29:42 -0500

    Simple setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from disutils.core import setup
+
+setup(
+    name='PLOD',
+    version='1.0',
+    description='Matplotlib plot designer',
+    author='David Kleiven',
+    author_email='davidkleiven446@gmail.com',
+    url='https://github.com/davidkleiven/PLOD',
+    py_modules=['plotHandler', 'controlGUI']
+)
